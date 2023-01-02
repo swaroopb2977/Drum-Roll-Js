7ef8494b95250d853220b2ffc84f7efaef68458f
Swaroop Bhanja <swaroopbhanja996@gmail.com>
2023-01-02 14:22:54 +0530

    added this keyword functionality

--- drum.js
+++ drum.js
@@ -11,7 +11,85 @@ document.querySelector(".set").addEventListener("click",function () {
 
 for (var i=0;i<document.querySelectorAll(".drum").length;i++)
 {
-    document.querySelector("button")[i].addEventListener("click",function () {
-        alert("I got clicked");
-    });
+    document.querySelectorAll("button")[i].addEventListener("click",Sound);
+
+    function Sound()
+    {
+        this.style.color="Orange";
+
+    }
 }
+/*
+
+
+if(i<4)
+        {
+            this.style.color="white";
+            var audio=new Audio("sounds/tom-"+(i+1)+".mp3");
+            audio.play();
+        }
+
+        else if(i==4)
+        {
+            var audio=new Audio("sounds/crash.mp3");
+            audio.play();
+        }
+        else if(i==5)
+       {
+            var audio=new Audio("sounds/kick-bass.mp3");
+            audio.play();
+        
+        }
+
+        else{
+            
+            this.style.color="white";
+                var audio=new Audio("sounds/snare.mp3");
+                audio.play();
+        }
+
+    
+
+
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////////
+    if(i<4)
+    {
+        function Sound()
+        {
+            var audio=new Audio("sounds/tom-"+(i+1)+".mp3");
+            audio.play();
+        }
+    }
+    else if(i===4)
+    {
+        function Sound()
+        {
+            var audio=new Audio("sounds/crash.mp3");
+            audio.play();
+        }
+    }
+    else if(i===5)
+    {
+        function Sound()
+        {
+            var audio=new Audio("sounds/kick-bass.mp3");
+            audio.play();
+        }
+    }
+    else{
+        function Sound()
+        {
+            var audio=new Audio("sounds/snare.mp3");
+            audio.play();
+        }
+    }
+   
+*/
+
